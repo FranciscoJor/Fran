5e0c8eb55888412ef0ad551eeb2101462c1e8c50
FranciscoJor <franciscoajorquerapardo@gmail.com>
2025-01-18 17:13:02 -0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import discord 
+import os
+import random
+from bot_logic import gen_pass
 from discord.ext import commands
 
 # La variable intents almacena los privilegios del bot
@@ -7,28 +10,56 @@ intents = discord.Intents.default()
 intents.message_content = True
 # Crear un bot en la variable cliente y transferirle los privilegios
 bot = commands.Bot(command_prefix='$', intents=intents)
-text = "Claro! Aquí tienes los comandos que puedes realizar: $datos -Muestra datos curiosos sobre el impacto ambiental ; $consejos -da consejos de como reducir basura" 
-
 
 @bot.event
 async def on_ready():
     print(f'Hemos iniciado sesión como {bot.user}')
 
 @bot.command()
-async def ayuda(ctx):
-    await ctx.send(text)
+async def hello(ctx):
+    await ctx.send("Hola")
+@bot.command()
+async def bye(ctx):
+    await ctx.send("😎")
+@bot.command()
+async def password(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command()
+async def repeat(ctx, times: int, content= "repeating..."):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+        
+f = open('text.txt', 'r', encoding='utf-8')
+text = f.read()
+print(text)
+f.close()
+
+@bot.command()
+async def meme_random(ctx):
+    meme = os.listdir("Image")
+    meme_alet = random.choice(meme)
+    
+    with open(f'Image/{meme_alet}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+    
+@bot.command()
+async def minecraft(ctx):
+    meme = os.listdir("minecraft")
+    meme_alet = random.choice(meme)
+    
+    with open(f'minecraft/{meme_alet}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+    
 
 
 
 
-bot.run("MTMzMDI1Njg4MTc1OTAzMTQ0MA.GUNIk3.bJHTRTsiBcDsN4UDS6PyEf9ZIE1zkvdcp3a84Y")
-
-
-
-
-
-
-
-
-
-
+bot.run("MTMyMjY1Mzc1NjYyOTcxMjkwNg.Gh3jQJ.Xgtbw1CmUB3u41Y08GTCBmEzsIjJKumtznGZE4")
